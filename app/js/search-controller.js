36095d097013e7617b4696a7959de232ca8061a7
Nick Owen <nowen@scottlogic.com>
2015-10-08 13:46:57 +0100

    simplify

--- app/js/search-controller.js
+++ app/js/search-controller.js
@@ -5,16 +5,14 @@
     .controller('SearchCtrl', ['$http', '$routeParams', '$location', 'bitcoinService',
       function($http, $routeParams, $location, bitcoinService) {
         var self = this;
-        self.hash = '';
-        var submitted = false;
 
         function getHash() {
           if (isHash(self.query)) {
-            self.hash = self.query;
+            return Promise.resolve(self.query);
           } else if(isHeight(self.query)) {
             return bitcoinService.fetchHash(self.query)
               .then(function(hash) {
-                self.hash = hash;
+                return hash;
               })
           } else {
             // Invalid search input
@@ -22,24 +20,35 @@
         }
 
         this.submit = function() {
-          submitted = true;
           getHash()
-            .then(function() {
-              return bitcoinService.fetchBlock(self.hash)
-                .then(function(block) {
-                  self.block = block;
-                  $location.path('/blocks/' + self.block.hash);
-                  self.query = '';
-                });
+            .then(function(hash) {
+              $location.path('/blocks/' + hash);
+              self.query = '';
             });
         };
 
         function isHash(input) {
-          return false;
+          if (input.length !== 64) {
+            return false;
+          }
+
+          var match = input.match(/^[a-f0-9]{64}$/);
+
+          if(match) {
+            return true;
+          } else {
+            return false;
+          }
         }
 
         function isHeight(input) {
-          return true;
+          var match = input.match(/^[0-9]+$/);
+
+          if(match) {
+            return true;
+          } else {
+            return false;
+          }
         }
       }]);
 })();
