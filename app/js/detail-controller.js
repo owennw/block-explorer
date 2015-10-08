116828118c614fd15290db8769403a1f3947ccb7
Nick Owen <nowen@scottlogic.com>
2015-10-08 14:14:41 +0100

    Add previous and next buttons to details page

--- app/js/detail-controller.js
+++ app/js/detail-controller.js
@@ -2,8 +2,8 @@
   'use strict';
 
   angular.module('blockChain.detail', ['blockChain.bitcoin'])
-    .controller('BlockDetailCtrl', ['$http', '$routeParams', 'bitcoinService',
-      function($http, $routeParams, bitcoinService) {
+    .controller('BlockDetailCtrl', ['$http', '$location', '$routeParams', 'bitcoinService',
+      function($http, $location, $routeParams, bitcoinService) {
         var self = this;
 
         self.blockHash = $routeParams.blockHash;
@@ -11,6 +11,16 @@
         bitcoinService.fetchBlock(self.blockHash)
           .then(function(block) {
             self.block = block;
+            self.previousDisabled = self.block.previousblockhash === undefined;
+            self.nextDisabled = self.block.nextblockhash === undefined;
           });
+
+        self.previous = function() {
+          $location.path('/blocks/' + self.block.previousblockhash);
+        }
+
+        self.next = function() {
+          $location.path('/blocks/' + self.block.nextblockhash);
+        }
       }]);
 })();
