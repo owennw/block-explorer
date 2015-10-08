8ac144bfe257f344f94cce342b1bcf6436a17c0c
Nick Owen <nowen@scottlogic.com>
2015-10-08 14:59:00 +0100

    View variable number of blocks

--- app/js/list-controller.js
+++ app/js/list-controller.js
@@ -5,21 +5,31 @@
     .controller('BlockListCtrl', ['$http', 'bitcoinService', function($http, bitcoinService) {
       var self = this;
       self.blocks = [];
-      var numberOfBlocksToFetch = 12;
+      self.viewOptions = [6, 10, 12, 16, 24, 48];
+      self.viewNumber = 12;
 
-      bitcoinService.fetchLatestBlock()
-        .then(function(block) {
-          self.blocks.push(block);
-          return block.previousblockhash;
-        })
-        .then(function(previousHash) {
-          fetchBlocks(previousHash, numberOfBlocksToFetch - 1);
-        });
+      display();
+
+      function display() {
+        bitcoinService.fetchLatestBlock()
+          .then(function (block) {
+            self.blocks.push(block);
+            return block.previousblockhash;
+          })
+          .then(function (previousHash) {
+            fetchBlocks(previousHash, self.viewNumber - 1);
+          });
+      }
 
       self.loadPrevious = function() {
         var nextBlockHash = self.blocks.slice(-1)[0].previousblockhash;
         self.blocks = [];
-        fetchBlocks(nextBlockHash, numberOfBlocksToFetch);
+        fetchBlocks(nextBlockHash, self.viewNumber);
+      };
+
+      self.viewChanged = function() {
+        self.blocks = [];
+        display();
       };
 
       function fetchBlocks(hash, count) {
