5eb17373347f5b0795a9b0393825ec43c2715a6d
Nick Owen <nowen@scottlogic.com>
2015-10-14 16:45:37 +0100

    Setup basic force graph, with data in the controller

--- app/js/forcegraph-directive.js
+++ app/js/forcegraph-directive.js
@@ -5,8 +5,101 @@
     .directive('txForceGraph', function () {
       return {
         restrict: 'E',
+        scope: {
+          nodes: '=',
+          links: '='
+        },
         link: function (scope, element, attrs) {
-          console.log('hello world');
+          var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+            width = 500 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+          var svg = d3.select(element[0]).append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom);
+
+          var radius = 5;
+
+          var nodes = scope.nodes;
+          var links = scope.links;
+
+          var force = d3.layout.force()
+            .size([width, height])
+            .linkDistance(20);
+
+          var render = function (nodes, links) {
+            force
+              .nodes(nodes)
+              .links(links);
+
+            var link = svg.selectAll('.link')
+              .data(links).enter()
+                .append('line')
+                .attr('class', 'link')
+                .attr('stroke', '#777');
+
+            var node = svg.selectAll('.node')
+              .data(nodes).enter()
+                .append('circle')
+                .attr('class', 'node');
+
+            force.on('tick', function () {
+              var animationDuration = 50;
+
+              node
+                .transition()
+                .ease('linear')
+                .duration(animationDuration)
+                .attr({
+                  cx: function (d) {
+                    return d.x;
+                  },
+                  cy: function (d) {
+                    return d.y;
+                  }
+                });
+
+              link
+                .transition()
+                .ease('linear')
+                .duration(animationDuration)
+                .attr({
+                  x1: function (d) {
+                    return d.source.x;
+                  },
+                  y1: function (d) {
+                    return d.source.y;
+                  },
+                  x2: function (d) {
+                    return d.target.x;
+                  },
+                  y2: function (d) {
+                    return d.target.y;
+                  }
+                });
+
+              force.stop();
+
+              setTimeout(
+                function () { force.start(); },
+                animationDuration);
+            });
+
+            force.on('end', function () {
+              node.attr('r', radius)
+                  .attr('cx', function (d) { return d.x; })
+                  .attr('cy', function (d) { return d.y; });
+
+              link.attr('x1', function (d) { return d.source.x; })
+                  .attr('y1', function (d) { return d.source.y; })
+                  .attr('x2', function (d) { return d.target.x; })
+                  .attr('y2', function (d) { return d.target.y; });
+            });
+
+            force.start();
+          };
+
+          render(scope.nodes, scope.links);
         }
       };
     });
--- app/js/transaction-controller.js
+++ app/js/transaction-controller.js
@@ -6,5 +6,13 @@
       var self = this;
       self.blockHash = $routeParams.blockHash;
       self.txHash = $routeParams.txHash;
+
+      self.nodes = [
+              { x: 500 / 3, y: 500 / 2 },
+              { x: 2 * 500 / 3, y: 500 / 2 }
+      ];
+      self.links = [
+            { source: 0, target: 1 }
+      ];
     }]);
 }());
